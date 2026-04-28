38a00a5e51bbf8f64475a477957d361d59efa65d
pedroyoshikadogarcia <pedroyoshikado@gmail.com>
2026-04-28 12:06:17 -0300

    Adição do } e das chamadas de funções das linhsa 4, 5, 6, 7

--- checkpoint2-dsa/src/main.c
+++ checkpoint2-dsa/src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
- 
+
+void calcularMedia();
+void calcularDerivada();
+void limparTela();
+
 int main() {
     int opcao;
  
@@ -94,4 +98,5 @@ void limparTela() {
         system("clear");
 
     #endif
+}
 
