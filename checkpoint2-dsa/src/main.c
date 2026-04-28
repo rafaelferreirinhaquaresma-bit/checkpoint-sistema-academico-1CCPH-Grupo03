c6a5126067885d8fb141c69e70a7d17c9387ba01
lucas123045 <lucaveiga12@gmail.com>
2026-04-28 11:43:56 -0300

    correção nome do grupo

--- checkpoint2-dsa/src/main.c
+++ checkpoint2-dsa/src/main.c
@@ -6,7 +6,7 @@ int main() {
     int opcao;
  
     do {
-        printf("--- SISTEMA ACADEMICO DSA ---\n");
+        printf("--- SISTEMA ACADEMICO GRUPO 3   ---\n");
         printf("1. Calcular Media e Classificacao\n");
         printf("2. Calcular Derivada (Funcao Simples)\n");
         printf("3. Sair\n");
