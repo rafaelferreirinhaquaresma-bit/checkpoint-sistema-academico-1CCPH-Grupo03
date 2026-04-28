20d91432f6f07c5f9a2816a149410a3ce61e933e
pedroyoshikadogarcia <pedroyoshikado@gmail.com>
2026-04-28 11:25:56 -0300

    Criação da funcao do calculo da derivada (calcularDerivada)

--- checkpoint2-dsa/src/main.c
+++ checkpoint2-dsa/src/main.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+void calcularDerivada() {
+    int a, b, c;
+    printf("Digite os coeficientes a, b e c: \n");
+    scanf("%d %d %d", &a, &b, &c); 
+    
+    printf("f'(x) = %dx + %d\n", 2 * a, b);
+}
 int main() {
     
 
